ce43d61c4843aee861ffd0ec6faeced37d670e5f
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-18 16:00:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('go swimming');
+
+//function expression
+function mondayWork(activity= 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('go to movies')
+
+//function wrapAdjective
+function wrapAdjective(visualFlair = "*"){
+    return function(adjective = "special"){
+      return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+  }
+  wrapAdjective("*")("a dedicated programmer")
